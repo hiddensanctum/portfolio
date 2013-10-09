240c7caf3aee4671ba962df19f824725a7336e19
James Chuang <jameschuang13@gmail.com>
2013-10-09 15:14:56 -0700

    get rid of extra project

--- blog/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = 'd6026bea44a8ea1ca3c930c857981cbd7b3b17bb2f3a2d146d3248d2a914db5f9732db59e76821d5a8611853a55946f3822d4d3a0999c0c9cb8214a996fd122f'
