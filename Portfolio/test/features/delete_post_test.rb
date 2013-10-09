4b552ed87dcc7c8ec2bf8c1c38c4146e0b3601bd
James Chuang <jameschuang13@gmail.com>
2013-10-09 16:15:59 -0700

    adds fixtures to the test

--- Portfolio/test/features/delete_post_test.rb
+++ Portfolio/test/features/delete_post_test.rb
@@ -1,26 +1,9 @@
 require "test_helper"
 
 feature "deleting a post" do
-  scenario "creating a post" do
-    # Create a post
+  scenario "delete the post" do
+    # Visit post index page
     visit posts_path
-    click_on 'New Post'
-    fill_in 'Title', with: 'Code Fellows'
-    fill_in 'Body', with: 'Its a good day for rails'
-
-    # When I submit the form
-    click_on 'Create Post'
-
-    # Then I should see the post
-    page.text.must_include 'Code Fellows'
-    page.text.must_include 'Its a good day for rails'
-
-    # And a success message
-    page.text.must_include 'Post was successfully created'
-
-    # Go back to index
-    click_on 'Back'
-
     # Click on destroy
     click_on 'Destroy'
 
--- Portfolio/test/features/edit_post_test.rb
+++ Portfolio/test/features/edit_post_test.rb
@@ -4,19 +4,6 @@ feature "editing a post" do
   scenario "makes a new post" do
     # Create a post
     visit posts_path
-    click_on 'New Post'
-    fill_in 'Title', with: 'Code Fellows'
-    fill_in 'Body', with: 'Its a good day for rails'
-
-    # When I submit the form
-    click_on 'Create Post'
-
-    # Then I should see the post
-    page.text.must_include 'Code Fellows'
-    page.text.must_include 'Its a good day for rails'
-
-    # And a success message
-    page.text.must_include 'Post was successfully created'
 
     # Given a edit post form
     click_on 'Edit'
