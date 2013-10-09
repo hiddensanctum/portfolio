a6b256c04cbb10785cbe5c32ea38e2da02242d3e
James Chuang <jameschuang13@gmail.com>
2013-10-09 14:13:51 -0700

    adds test that tests editing an existing post

--- blog/test/features/edit_post_test.rb
+++ blog/test/features/edit_post_test.rb
@@ -1,3 +1,40 @@
 require "test_helper"
 
+feature "editing a post" do
+  scenario "makes a new post" do
+    # Create a post
+    visit post_path
+    click_on 'New Post'
+    fill_in 'Title', with: 'Code Fellows'
+    fill_in 'Content', with: 'Its a good day for rails'
 
+    # When I submit the form
+    click_on 'Create Post'
+
+    # Then I should see the post
+    page.text.must_include 'Code Fellows'
+    page.text.must_include 'Its a good day for rails'
+
+    # And a success message
+    page.text.must_include 'Post was successfully created'
+
+    scenario 'change existing post' do
+
+      # Given a edit post form
+      click_on 'Edit'
+      fill_in 'Title', with: 'Code Fellows Day 3'
+      fill_in 'Content', with: 'Not a good day for rails'
+
+      # When I submit the form
+      click_on 'Edit Post'
+
+      # I should see the changes
+      page.text.must_include 'Code Fellows Day 3'
+      page.text.must_include 'Not a good day for rails'
+
+      # Show successful change message
+      page.text.must_include 'Post was successfully edited'
+
+    end
+  end
+end
