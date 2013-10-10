91ef91a840d5a8b70a9335bcff320d59e2db7c2c
James Chuang <jameschuang13@gmail.com>
2013-10-09 18:54:40 -0700

    adds minitest with rails assignment

---
+++ store/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'd9156e27da6831c5388e5fd61f93da2cdc8f70e568b546647aab92a0f2322a0593a8f57e9f4f887dbd1df03bceba95c6e01cc96ca84025fd2865161da4cb263b'
