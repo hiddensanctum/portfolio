0cdf64732680d18e596aeb1a7708c8c0d2ba3850
James Chuang <jameschuang13@gmail.com>
2013-10-14 19:27:32 -0700

    combine code of unhappy test to create project test

--- test/features/projects/create_project_test.rb
+++ test/features/projects/create_project_test.rb
@@ -12,4 +12,16 @@ require 'test_helper'
       page.text.must_include "Manhattan Project"
       page.text.must_include "Nuclear"
     end
+  scenario "new project has invalid data" do
+    visit new_project_path
+    fill_in "Name", with: "Q"
+
+    click_on "Create Project"
+
+    current_path.must_match /projects$/
+    page.text.must_include "Project could not be saved"
+    page.text.must_include "Name is too short"
+    page.text.must_include "Technologies used can't be blank"
+
   end
+end
