cfb927212ffcfef9a3e55017a89f310cfba8cc48
James Chuang <jameschuang13@gmail.com>
2013-10-24 01:04:23 -0700

    fix heroku migration

--- db/migrate/20131023205752_add_commentable_to_comments.rb
+++ db/migrate/20131023205752_add_commentable_to_comments.rb
@@ -2,6 +2,5 @@ class AddCommentableToComments < ActiveRecord::Migration
   def change
     add_column :comments, :commentable_id, :integer
     add_column :comments, :commentable_type, :string
-    remove_column :comments, :post_id, :integer
   end
 end
