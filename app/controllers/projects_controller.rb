6c364334bc26487aff4f217328366e2347fd4c07
James Chuang <jameschuang13@gmail.com>
2013-11-17 01:27:47 -0800

    get rid of unneccesary instance variables for projecs controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,9 +26,6 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
-    @commentable = @project
-    @comments = policy_scope(@project.comments)
-    @comment = Comment.new
   end
 
   def edit
