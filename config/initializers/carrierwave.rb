7f1ccdf93947454c59ad4f4d36b767d8caf6bcb4
James Chuang <jameschuang13@gmail.com>
2013-11-18 23:02:39 -0800

    add image caching for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,11 @@
+require ::File.expand_path('../config/environment',  __FILE__)
+use Rack::Static, :urls => ['/carrierwave'], :root => 'tmp' # adding this line
+run YourApplicationName::Application
+
 CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+
   config.fog_credentials = {
     provider: "AWS",
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
