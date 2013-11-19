954c203623003fd7ec1c6a1f3d7eb80047478683
James Chuang <jameschuang13@gmail.com>
2013-11-18 23:08:02 -0800

    move code to config.ru

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,3 @@
-require ::File.expand_path('../config/environment',  __FILE__)
-use Rack::Static, :urls => ['/carrierwave'], :root => 'tmp' # adding this line
-run YourApplicationName::Application
-
 CarrierWave.configure do |config|
   config.root = Rails.root.join('tmp')
   config.cache_dir = "#{Rails.root}/tmp/uploads"
